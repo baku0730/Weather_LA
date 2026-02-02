987a541a090cbd971b1456d1cede3b4dfd654c5d
Baku-Singh <pupil.purple.730@gmail.com>
2026-02-02 20:42:10 +0900

    Initial Commit

---
+++ Weather_LA/Weather_LAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_LAApp.swift
+//  Weather_LA
+//
+//  Created by シング　シュバクシ on 2026/02/02.
+//
+
+import SwiftUI
+
+@main
+struct Weather_LAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather_LATests/Weather_LATests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_LATests.swift
+//  Weather_LATests
+//
+//  Created by シング　シュバクシ on 2026/02/02.
+//
+
+import Testing
+@testable import Weather_LA
+
+struct Weather_LATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
